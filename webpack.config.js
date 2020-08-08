22f2b2d01de3c0ae71857a3c3cb8c0ac8ca344b0
jacobguenther <jacobrwguenther@protonmail.com>
2020-08-08 12:13:34 -0800

    Add MiniCssExtractPlugin.

--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,40 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
 	mode: "development",
-    entry: {
+	entry: {
 		main: './index.js'
 	},
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js',
-        libraryTarget: 'var',
-        library: 'RollLang',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Roll Lang',
-            filename: 'index.html',
-            template: 'index.html',
-            scriptLoading: 'defer',
-        }),
-        new WasmPackPlugin({
-            crateDirectory: path.resolve(__dirname, "."),
-            // args: "",
-            // extraArgs: "",
-            // outDir: "pkg",
-            // outName: "index",
-            // forceWatch: true,
-        }),
-
-        new webpack.ProvidePlugin({
-          TextDecoder: ['text-encoding', 'TextDecoder'],
-          TextEncoder: ['text-encoding', 'TextEncoder']
-        })
-    ],
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'index.js',
+		libraryTarget: 'var',
+		library: 'RollLang',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: [MiniCssExtractPlugin.loader, 'css-loader'],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Roll Lang',
+			template: 'index.html',
+			scriptLoading: 'defer',
+		}),
+		new MiniCssExtractPlugin(),
+		new WasmPackPlugin({
+			crateDirectory: path.resolve(__dirname, "."),
+		}),
+		new webpack.ProvidePlugin({
+		  TextDecoder: ['text-encoding', 'TextDecoder'],
+		  TextEncoder: ['text-encoding', 'TextEncoder']
+		})
+	],
 };
