44b8324063bda5fdfefca49e711290e898b11b5e
jacobguenther <jacobrwguenther@protonmail.com>
2020-08-21 22:36:33 -0800

    Add first unit tests.

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,40 @@
 
 extern crate unicode_segmentation;
 
-mod lexer;
-mod parser;
+pub mod lexer;
+pub mod parser;
 pub mod ast;
 pub mod interpreter;
 pub mod macros;
 
-#[cfg(feature = "web")]
-pub mod wasm_interface;
-#[cfg(feature = "web")]
-pub mod web;
+#[cfg(test)]
+pub mod tests {
+	use std::collections::HashMap;
+	use super::*;
+	use super::interpreter::*;
+	use super::interpreter::output_traits::*;
+	use super::macros::MacroData;
+
+	fn helper(source: &str, result: &str, macros: &HashMap<String, MacroData>) {
+		let output = interpreter::Interpreter::new(&source, &macros)
+			.interpret()
+			.as_string();
+		assert_eq!(&output, result);
+	}
+	
+	#[test]
+    fn interpreter() {
+		// associativity
+		helper("[[5-4+1]]", "5-4+1=2", &HashMap::new());
+		// multiply and divide
+		helper("[[4*6/3]]", "4*6/3=8", &HashMap::new());
+		// precedence
+		helper("[[(4+2)*2]]", "(4+2)*2=12", &HashMap::new());
+
+		// unicode and localization
+		helper("文字 hello", "文字 hello", &HashMap::new());
+
+		// whitespaces
+		helper("attack is [[20+1]] and damage is [[10]]", "attack is 20+1=21 and damage is 10=10", &HashMap::new());
+	}
+}
