dfd66fc12b70d43ca7d8c1472ae0b0d3c4dae170
jacob <jacobrwguenther@protonmail.com>
2022-07-09 22:39:50 -0800

    add tests, check errors on dice with floats for side and/or count.

---
+++ src/tests/computed_dice.rs
@@ -0,0 +1,40 @@
+// File: tests/mod.rs
+
+use super::*;
+
+#[test]
+fn errors() {
+	let output = helper_return_result(r, "/r (1.0)d(10)");
+	println!("{:?}", output);
+	match output.error {
+		Some(InterpretError::DiceCountMustBeAnInteger) => (),
+		Some(e) => panic!(
+			"expected error InterpretError::DiceCountMustBeAnInteger got {:?}",
+			e
+		),
+		None => panic!("expected error InterpretError::DiceCountMustBeAnInteger got none"),
+	}
+
+
+	let output = helper_return_result(r, "/r (10)d(1.0)");
+	println!("{:?}", output);
+	match output.error {
+		Some(InterpretError::DiceSidesMustBeAnInteger) => (),
+		Some(e) => panic!(
+			"expected error InterpretError::DiceSidesMustBeAnInteger got {:?}",
+			e
+		),
+		None => panic!("expected error InterpretError::DiceSidesMustBeAnInteger got none"),
+	}
+
+	let output = helper_return_result(r, "/r (1.0)d(1.0)");
+	println!("{:?}", output);
+	match output.error {
+		Some(InterpretError::DiceCountMustBeAnInteger) => (),
+		Some(e) => panic!(
+			"expected error InterpretError::DiceCountMustBeAnInteger got {:?}",
+			e
+		),
+		None => panic!("expected error InterpretError::DiceCountMustBeAnInteger got none"),
+	}
+}
